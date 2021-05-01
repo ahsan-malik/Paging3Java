d5678a9e8bcbbd638aca59bb4b8253bb454cdf85
ahsan-malik <malikahsan2001@gmail.com>
2021-05-01 13:01:14 +0500

    glide

--- app/src/main/java/com/ahsanmalik/paging3java/appUtils/BindingAdapters.java
+++ app/src/main/java/com/ahsanmalik/paging3java/appUtils/BindingAdapters.java
@@ -4,13 +4,18 @@ import android.widget.ImageView;
 
 import androidx.databinding.BindingAdapter;
 
+import com.ahsanmalik.paging3java.R;
 import com.bumptech.glide.Glide;
 
 public class BindingAdapters {
 
     @BindingAdapter("loadImage")
     public static void setImage(ImageView imageView, String url){
-        Glide.with(imageView).load(url).into(imageView);
+        Glide.with(imageView)
+                .load(url)
+                .error(R.drawable.ic_twotone_error_24)
+                .placeholder(R.drawable.ic_twotone_image_24)
+                .into(imageView);
     }
 
 }
